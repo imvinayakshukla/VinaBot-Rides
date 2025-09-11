88276028c26d251db8ae148d0353aca8b2d1e66b
Vinayak Shukla <vinayakshukla0786@gmail.com>
2025-09-11 22:07:29 +0530

    new: added chatbot template

--- frontend/vinabot-rides-app/src/app/app.ts
+++ frontend/vinabot-rides-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Chatbot } from "./chatbot/chatbot";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Chatbot],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
